bea01339d09f4ba8dcfcc2883e71b44be1654dca
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-31 13:18:12 -0400

    git

--- main.ts
+++ main.ts
@@ -2,7 +2,45 @@
  *
  * Created by: sophie
  * Created on: oct 2023
- * This program ...
+ * This program moves a pixel LED
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+let sprite: game.LedSprite = null
+let loopCounter: number = 4
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Ghost)
+pause(500)
+basic.clearScreen()
+
+// neopixel setup
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+
+// button A pressed
+input.onButtonPressed(Button.A, function() {
+  // setup
+  basic.clearScreen()
+  let loopCounter = 4
+
+  // loop
+  while (loopCounter >= 0) {
+    basic.pause(500)
+    basic.showNumber(loopCounter)
+    neopixelStrip = neopixel.create(DigitalPin.P16, loopCounter, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    loopCounter = loopCounter - 1
+  }
+})
